f23111cbd232e3877499a730a720d1398201dbce
Juan Lara <julara@unal.edu.co>
2025-01-14 16:19:45 -0500

    feat(default): initial commit

---
+++ dot_config/ipython/profile_default/ipython_config.py
@@ -0,0 +1,9 @@
+config = get_config()
+
+config.TerminalIPythonApp.display_banner = True
+config.TerminalInteractiveShell.confirm_exit = False
+config.TerminalInteractiveShell.editing_mode = "vi"
+config.TerminalInteractiveShell.editor = "nvim"
+config.TerminalInteractiveShell.true_color = True
+config.TerminalInteractiveShell.autoindent = False
+config.TerminalInteractiveShell.auto_match = False
